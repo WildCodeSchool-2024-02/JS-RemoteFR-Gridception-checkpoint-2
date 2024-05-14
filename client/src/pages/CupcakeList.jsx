afe5e38013b7c9f5ae529c54c5794e9d771f3133
NICOLAS LOMBA <nicolaslomba82@gmail.com>
2024-05-14 11:52:01 +0200

    step 2

--- client/src/pages/CupcakeList.jsx
+++ client/src/pages/CupcakeList.jsx
@@ -40,6 +40,8 @@ someCupcakes.push(
 function CupcakeList() {
   // Step 1: get all cupcakes
   console.info(useLoaderData());
+  const cupcakes = useLoaderData();
+
 
   // Step 3: get all accessories
 
@@ -60,6 +62,13 @@ function CupcakeList() {
       </form>
       <ul className="cupcake-list" id="cupcake-list">
         {/* Step 2: repeat this block for each cupcake */}
+
+        { cupcakes.map((cupcake) => (
+            <div key={cupcake.id} > 
+            <Cupcake data={cupcake} />       
+            </div>
+          ))}
+
         {/* Step 5: filter cupcakes before repeating */}
         <li className="cupcake-item">
           <Cupcake />
