68a7f5682c55821beaa571e2ed492a3198ff2f7b
Lauryn MARTIN <lauryn.martin333@gmail.com>
2024-05-14 11:07:07 +0200

    Step 4 done

--- client/src/pages/CupcakeList.jsx
+++ client/src/pages/CupcakeList.jsx
@@ -67,6 +67,11 @@ function CupcakeList() {
           <select id="cupcake-select">
             <option value="">---</option>
             {/* Step 4: add an option for each accessory */}
+            {accessories.map((accessorie) => (
+              <option key={accessories.id} value={accessorie.id}>
+                {accessorie.name}
+              </option>
+            ))}
           </select>
         </label>
       </form>
