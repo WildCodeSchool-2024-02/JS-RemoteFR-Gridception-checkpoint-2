e5eada6189fe92a20c3966d7d206fa0dfc632a3d
Lauryn MARTIN <lauryn.martin333@gmail.com>
2024-05-14 09:47:18 +0200

    Step 2 done

--- client/src/pages/CupcakeList.jsx
+++ client/src/pages/CupcakeList.jsx
@@ -40,6 +40,7 @@ someCupcakes.push(
 function CupcakeList() {
   // Step 1: get all cupcakes
   console.info(useLoaderData());
+  const data = useLoaderData();
 
   // Step 3: get all accessories
 
@@ -60,6 +61,9 @@ function CupcakeList() {
       </form>
       <ul className="cupcake-list" id="cupcake-list">
         {/* Step 2: repeat this block for each cupcake */}
+        {data.map((cupcake) => (
+          <Cupcake key={cupcake.id} data={cupcake} />
+        ))}
         {/* Step 5: filter cupcakes before repeating */}
         <li className="cupcake-item">
           <Cupcake />
